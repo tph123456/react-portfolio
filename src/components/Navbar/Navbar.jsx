c233d8e30ff4331f371d58db7cd261c3058b4886
Tatum Hameister <tatumhameister@gmail.com>
2024-11-22 13:14:41 +1100

    changing portfolio name to TH

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
     
     return <nav className={styles.navbar}>
-        <a className={styles.title} href="/">My Portfolio</a>
+        <a className={styles.title} href="/">TH</a>
         <div className={styles.menu}>
             <img className={styles.menuBtn} 
             src={menuOpen ? getImageUrl("nav/closeIcon.png") 
